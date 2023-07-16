314eeb7bfd9eacc0699492c7371e75721b8625b4
ls-da3m0ns <backbencherspot@gmail.com>
2023-07-17 01:10:49 +0530

    clean up

--- quadkey/__init__.py
+++ quadkey/__init__.py
@@ -1,7 +1,7 @@
 from .quadkey import *
 
-__version__='1.0.0'
-__author__='Prashant Singh'
+__version__ = "1.0.1"
+__author__ = "Prashant Singh"
 
 # Path: python/quadkey-boosted/quadkey/utils.pyx
 
--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="quadkey",
-    version="1.0.0",
+    name="quadkey-boosted",
+    version="1.0.1",
     author="Prashant Singh",
+    author_email="mail.prashantsingh.41@gmail.com",
     description="Python library that provides a powerful set of tools and functions for working with quadkeys. Built on top of C it offers lightning-fast calculations, ensuring optimal performance even with large-scale datasets.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,9 +22,30 @@ setup(
             Extension("quadkey.core.utils", 
                 sources=["quadkey/core/utils.pyx"], 
                 language="c", optional=False, include_dirs=[get_python_inc()]) ]),
-    packages=find_packages(),
+    packages=find_packages('quadkey'),
     include_dirs=[get_python_inc()],
     install_requires=[
         "Cython",
-    ]
+    ],
+    package_dir={'': 'quadkey'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+    project_urls={
+        'Documentation': 'https://github.com/lsda3m0ns/pyquadkey-boosted',
+        'Source Code': 'https://github.com/lsda3m0ns/pyquadkey-boosted',
+        'Bug Tracker': 'https://github.com/lsda3m0ns/pyquadkey-boosted/issues',
+    },
+    entry_points={
+        'console_scripts': [
+            'quadkey = quadkey.__main__:main',
+        ],
+    },
+    ext_package="quadkey",
+
 )
